56c92c87e5cadcd2385e82fd345b8deb3bddccd0
Daniel No <>
2021-07-13 15:20:25 -0400

    add file structure

---
+++ CombineDemo/CombineDemo/Classes/NetworkManager.swift
@@ -0,0 +1,15 @@
+//
+//  NetworkManager.swift
+//  CombineDemo
+//
+//  Created by Daniel No on 7/13/21.
+//
+
+import Foundation
+import Combine
+
+public class NetworkManager{
+    
+    
+    
+}
---
+++ CombineDemo/CombineDemo/View Controllers/InterfaceBindingViewController.swift
@@ -0,0 +1,29 @@
+//
+//  InterfaceBindingViewController.swift
+//  CombineDemo
+//
+//  Created by Daniel No on 7/13/21.
+//
+
+import UIKit
+
+class InterfaceBindingViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
