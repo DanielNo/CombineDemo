c940273a4798b1194ab056fc1b7d36f952b2645d
Daniel No <>
2021-07-14 17:28:38 -0400

    downstream publisher works

--- CombineDemo/CombineDemo/View Controllers/ReactiveViewController.swift
+++ CombineDemo/CombineDemo/View Controllers/ReactiveViewController.swift
@@ -11,26 +11,21 @@ import Combine
 class ReactiveViewController: UIViewController,UITextFieldDelegate {
     @IBOutlet weak var usernameTextField: UITextField!
     @IBOutlet weak var passwordTextField: UITextField!
-    
-    @Published var usernameText : String = ""
-    @Published var passwordText : String = ""
-    
     @IBOutlet weak var submitButton: UIButton!
-    // Keep a reference to the subscription so it's
-    // only cancelled when we are deallocated.
+    
+    
+    // Combine
+    // Keep a single reference to the subscription so it's only cancelled when we are deallocated.
     private var usernameSubscription: AnyCancellable?
     private var passwordSubscription: AnyCancellable?
+    private var validatedUserAndPasswordSubscription: AnyCancellable?
 
+    // Another option: Store multiple subscriptions in a collection
     var subscriptions : Set<AnyCancellable> = []
     
     var usernameSubject : CurrentValueSubject<String,Never> = CurrentValueSubject("")
     var passwordSubject : CurrentValueSubject<String,Never> = CurrentValueSubject("")
-
-//    var validPasswordSubject : CurrentValueSubject<Bool,Never> = CurrentValueSubject(false)
-//    var validUsernameSubject : CurrentValueSubject<Bool,Never> = CurrentValueSubject(false)
-
     
-    private var stream: AnyCancellable?
 
 
     override func viewDidLoad() {
@@ -41,26 +36,20 @@ class ReactiveViewController: UIViewController,UITextFieldDelegate {
     }
     
     func createPipeline(){
-        self.usernameSubscription = usernameSubject.filter({ str in
-            return str.count > 5
-        })
+        usernameSubject
         .sink { completion in
             print(completion)
         } receiveValue: { username in
             print("username : \(username)")
-        }
-        
-        let valid = usernameSubject.map { return $0        }.sink { completion in
-            print(completion)
-        } receiveValue: { isValid in
-            print("is valid username : \(isValid)")
-        }
+        }.store(in: &subscriptions)
 
-        stream = Publishers.CombineLatest(usernameSubject, passwordSubject).map { a,b in
-            return a.count > 5 && b.count > 5
+        validatedUserAndPasswordSubscription = Publishers.CombineLatest(usernameSubject, passwordSubject).map { user,pass in
+            return user.count > 5 && pass.count > 5
         }.eraseToAnyPublisher().sink(receiveValue: { isValid in
             print("username and password are valid :\(isValid)")
+            self.submitButton.isEnabled = isValid
         })
+        
         
     }
     
@@ -75,9 +64,9 @@ class ReactiveViewController: UIViewController,UITextFieldDelegate {
         }
     }
     @IBAction func signupBtnPressed(_ sender: Any) {
-        print("username : \(usernameSubject.value)")
-        print("password : \(passwordSubject.value)")
-
+        print("Signing up with username : \(usernameSubject.value)")
+        print("Signing up with password : \(passwordSubject.value)")
+        
     }
     
     /*
